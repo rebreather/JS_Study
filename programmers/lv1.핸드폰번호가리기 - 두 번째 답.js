7459649eb714d2a7cdce9d1b740198e0db8b340f
rebreather <yds04116@gmail.com>
2022-01-29 01:08:52 +0900

    220128 - lv1. 핸드폰번호 가리기

---
+++ programmers/lv1.핸드폰번호가리기 - 두 번째 답.js
@@ -0,0 +1,11 @@
+﻿function solution(phone_number) {
+    var answer = '';
+    var sliced = phone_number.slice(-4); //맨 끝 4자리
+    var changingnum = phone_number.slice(0,-4); //*로 치환될 번호
+    
+    for(var i = 0; i < changingnum.length; i++) {
+        changingnum = changingnum.replace(changingnum[i],"*");
+    }
+    answer = changingnum + sliced;
+    return answer;
+}
---
+++ programmers/lv1.핸드폰번호가리기 - 첫 번째 답.js
@@ -0,0 +1,16 @@
+﻿function solution(phone_number) {
+    var unsliced = '';
+    var num = phone_number.split(""); //['0','1',...]
+    var count = 0;
+    var sliced = num.slice(0,-4); // [0,1,0,6,2,7,9]
+    var remained = num.slice(-4); //[2,6,7,9]
+    
+    count = sliced.length; //잘려진 배열의 길이
+    
+    for(var i = 0; i < count; i++) {
+        remained.unshift('*'); 
+        //잘려진 배열의 길이만큼 남아있는 remained 배열에 * 넣기
+    }
+    sliced = remained.join("");
+    return sliced;
+}
