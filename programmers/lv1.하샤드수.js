47e7046f8f219db8df66130ce902b6d37409e8dd
rebreather <yds04116@gmail.com>
2022-01-29 17:42:20 +0900

    220129 - lv1.하샤드 수

---
+++ programmers/lv1.하샤드수.js
@@ -0,0 +1,17 @@
+﻿/*
+    ===생각한 풀이과정===
+    숫자를 문자열로 변환한 뒤 split으로 잘라내고 각 인덱스의 값의 총합을 구한다.
+    x를 총합으로 나누었을 때 0이면 true반환하고, 아니면 false를 반환한다.
+*/
+function solution(x) {
+    var answer = true;
+    var sum = 0;
+    var arrx = x.toString().split("");
+    for(var i = 0; i< arrx.length; i++) {
+        sum += Number(arrx[i]);
+    }
+    if((x % sum) == 0)
+        return answer;
+    else
+        return !answer;
+}
