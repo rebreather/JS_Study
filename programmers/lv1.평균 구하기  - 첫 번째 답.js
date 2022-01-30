e4a86a6933c99ed2713eac181abaa532ab681bcd
rebreather <yds04116@gmail.com>
2022-01-30 15:03:22 +0900

    220130 - lv1. 평균 구하기

---
+++ programmers/lv1.평균 구하기  - 첫 번째 답.js
@@ -0,0 +1,10 @@
+﻿function solution(arr) {
+    var answer = 0;
+    var sum = 0;
+    arr.forEach(n => {
+        sum += n;
+    })
+    answer = sum/arr.length;
+    
+    return answer;
+}
---
+++ programmers/lv1.평균 구하기 - 두 번째 답.js
@@ -0,0 +1,6 @@
+﻿function solution(arr) {
+    var answer = 0;
+    var sum = arr.reduce((acc,i) => acc + i, 0);
+    answer = sum/arr.length;
+    return answer;
+}
