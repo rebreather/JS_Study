48832859e562cbfb09b5c67bb42b8a8bea8da095
rebreather <yds04116@gmail.com>
2022-02-02 01:05:02 +0900

    220201 - lv1.최소공배수와 최대공약수

---
+++ programmers/lv1. 최대공약수와 최소공배수.js
@@ -0,0 +1,11 @@
+﻿function solution(n, m) {
+    var answer = [];
+    var greatest = (a, b) => {  
+        if (b === 0) {
+            return a
+        }               
+        return greatest(b, a % b)  
+    }
+    var least = (a, b) => (a*b) / greatest(a,b)
+    return [greatest(n,m), least(n,m)];
+}
