0edbd4e7d6afa30f35b093a23822b0bc075d62cb
rebreather <yds04116@gmail.com>
2022-02-07 19:01:01 +0900

    220207 - lv1. 자릿수 더하기

---
+++ programmers/lv1.자릿수 더하기.js
@@ -0,0 +1,8 @@
+﻿function solution(n) {
+    var answer = 0;
+    let arr = n.toString().split("").map(Number);
+    for(let i = 0; i<arr.length; i++) {
+        answer +=arr[i];
+    }
+    return answer;
+}
