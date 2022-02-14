71bf959569474b9661896643b618ac8a2895f021
rebreather <yds04116@gmail.com>
2022-02-15 00:05:04 +0900

    220214 - lv1. 서울에서 김서방 찾기

---
+++ programmers/lv1.서울에서 김서방 찾기.js
@@ -0,0 +1,9 @@
+﻿function solution(seoul) {
+    var answer = '김서방은 ';
+    for(let i = 0; i<seoul.length; i++) {
+        if(seoul[i] === "Kim")
+            answer += i;
+    }
+    answer +='에 있다';
+    return answer;
+}
