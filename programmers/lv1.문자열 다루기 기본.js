6e8f7eeb8ee74bad5f2d8f75b0c5fb0e08f94ce4
rebreather <yds04116@gmail.com>
2022-02-15 21:38:08 +0900

    220215 - lv1. 문자열 다루기 기본

---
+++ programmers/lv1.문자열 다루기 기본.js
@@ -0,0 +1,11 @@
+﻿function solution(s) {
+    let result = parseInt(s);
+    let answer = true;
+    console.log(result);
+    if ((s.length === 4 || s.length === 6) && s == result) {
+      answer = true;
+    } else {
+      answer = false;
+    }
+    return answer;
+}
