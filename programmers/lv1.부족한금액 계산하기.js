e1c5d9bb8c77f3b1c5335bb0a217e2cc5a9b0876
rebreather <yds04116@gmail.com>
2022-02-25 21:47:45 +0900

    220225 - lv1. 부족한 금액 계산하기

---
+++ programmers/lv1.부족한금액 계산하기.js
@@ -0,0 +1,9 @@
+﻿function solution(price, money, count) {
+    let total = 0;
+    let answer = 0;
+    for(let i = 1; i <= count; i++) {
+        total += (price*i);
+    }
+    answer = (money - total) > 0 ? 0 : (total - money);
+    return answer;
+}
