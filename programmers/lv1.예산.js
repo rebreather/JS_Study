4040a038783e6d03afaed91c12f331e74275b435
rebreather <yds04116@gmail.com>
2022-03-02 22:07:11 +0900

    220302 - lv1. 예산

---
+++ programmers/lv1.예산.js
@@ -0,0 +1,11 @@
+﻿function solution(d, budget) {
+    let sum = 0;
+        answer = 0;
+    let dpment = d.sort((a,b) => a-b);
+    dpment.forEach((e) => {
+        sum +=e;
+        if(sum <= budget)
+            answer++;
+    });
+    return answer;
+}
