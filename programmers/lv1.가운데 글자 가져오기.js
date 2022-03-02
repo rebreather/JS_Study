793e68a6fa23fa035f43d319c89bc750be4c5d84
rebreather <yds04116@gmail.com>
2022-03-03 02:36:41 +0900

    220303 - lv1. 가운데 글자 가져오기

---
+++ programmers/lv1.가운데 글자 가져오기.js
@@ -0,0 +1,19 @@
+﻿function solution(s) {
+    let answer = '';
+    let arr = s.split('');
+    if(s.length % 2 === 1) { // 문자열 길이가 홀수일 때
+        for(let i = 0; i<parseInt(s.length/2); i++) {
+            arr.pop();
+            arr.shift();
+        }
+        answer = arr.join('');
+    }
+    else { //짝수
+        for(let i = 0; i<(s.length/2) - 1; i++) {
+            arr.pop();
+            arr.shift();
+        }
+        answer = arr.join('');
+    }
+    return answer;
+}
