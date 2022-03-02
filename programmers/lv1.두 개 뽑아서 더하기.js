22bca0df74a7e6d130e05cea33ab275ab1e2b6e0
rebreather <yds04116@gmail.com>
2022-03-02 23:09:50 +0900

    220302 - lv1. 두 개 뽑아서 더하기

---
+++ programmers/lv1.두 개 뽑아서 더하기.js
@@ -0,0 +1,13 @@
+﻿function solution(numbers) {
+    let arr = [];
+        answer = [];
+    for(let i = 0; i<numbers.length; i++) {
+        for(let j = i+1; j<numbers.length; j++) {
+            arr.push(numbers[i] + numbers[j]);
+        }
+    }
+    answer = arr.filter((value, i) => { //arr배열에서 중복되는 원소 제거
+        return arr.indexOf(value) === i;
+    });
+    return answer.sort((a, b) => a-b); //오름차순으로 재배열
+}
