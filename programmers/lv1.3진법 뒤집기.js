182ad4d7971054d64d1e22e1c954d72050070ce0
rebreather <yds04116@gmail.com>
2022-03-03 18:04:06 +0900

    220303 - lv1. 3진법 뒤집기

---
+++ programmers/lv1.3진법 뒤집기.js
@@ -0,0 +1,8 @@
+﻿function solution(n) {
+    var answer = 0;
+    let trit = n.toString(3).split('').reverse().join('');
+    for(let i = 0; i < trit.length; i++) {
+        answer += (trit[i]*3**(trit.length-1-i));
+    }
+    return answer;
+}
