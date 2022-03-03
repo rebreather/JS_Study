97ddbea3708170806d8fbd458f76602e292ca573
rebreather <yds04116@gmail.com>
2022-03-03 21:45:23 +0900

    220303 - lv1. 약수의 개수와 덧셈

---
+++ programmers/lv1.약수의 개수와 덧셈.js
@@ -0,0 +1,16 @@
+﻿function solution(left, right) {
+    var answer = 0;
+    let numArr = [];
+    for(let i = left; i<=right; i++) { //left부터 right까지
+        for(let j = 1; j<=i; j++) { // 나눠야 할 수
+            if(i%j === 0) //약수 찾기
+                numArr.push(j);
+        }
+        if(numArr.length % 2 === 0) //약수의 갯수가 짝수일 때
+            answer += i;
+        else
+            answer -= i;
+        numArr = [];
+    }
+    return answer;
+}
