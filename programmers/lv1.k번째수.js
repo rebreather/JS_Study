c16d4c80ea79b2e4a95d2fdf22390d6d0c892b45
rebreather <yds04116@gmail.com>
2022-03-10 22:39:16 +0900

    220310 - lv1. k번째 수

---
+++ programmers/lv1.k번째수.js
@@ -0,0 +1,10 @@
+function solution(array, commands) {
+    var answer = [];
+    let arr = [];
+    for(let i = 0; i < commands.length; i++) {
+        arr = array.slice(commands[i][0]-1, commands[i][1]);
+        arr.sort((a, b) => a - b);
+        answer.push(arr[commands[i][2]-1]);
+    }
+    return answer;
+}
